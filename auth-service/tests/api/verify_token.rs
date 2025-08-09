4739743b7faad952c6b73b805b5b8cddbc0e4bb3
WallyOxen <wally@wallys.world>
2025-08-08 21:02:34 -0400

    [Sprint 3] [Task 4] Implement verify token route

--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,80 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
+use auth_service::utils::constants::JWT_COOKIE_NAME;
+use serde_json::json;
 
 #[tokio::test]
-async fn verify_token_returns_200_status() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_verify_token().await;
+    let test_cases = [
+        json!({
+            "token": true
+        }),
+        json!({}),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_verify_token(&test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_200_valid_token() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
 
     assert_eq!(response.status().as_u16(), 200);
+
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+
+    assert!(!auth_cookie.value().is_empty());
+
+    let response = app
+        .post_verify_token(&json!({
+            "token": auth_cookie.value()
+        }))
+        .await;
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    let response = app
+        .post_verify_token(&json!({
+            "token": "abc123"
+        }))
+        .await;
+
+    assert_eq!(response.status().as_u16(), 401);
 }
